09900589f3ac5c9ca040fbbf44113012756ff635
Rita de Cássia Souza de Moura <20201011110020@ifrn.local>
2023-04-11 17:59:44 -0300

    atuali

---
+++ progamaweb/progama/Controllers/categoriasController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using progama.Models;
+
+namespace progama.Controllers
+{
+    public class categoriasController : Controller
+    {
+        private static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria(){Nome = "ação", CategoriaId = 1},
+            new Categoria(){Nome = "ação", CategoriaId = 2},
+            new Categoria(){Nome = "ação", CategoriaId = 3},
+            new Categoria(){Nome = "ação", CategoriaId = 4},
+            new Categoria(){Nome = "ação", CategoriaId = 5}
+        };
+
+
+        // GET: categorias
+        public ActionResult Index()
+        {
+            return View(categorias);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            categoria.CategoriaId = categorias.Select(m => m.CategoriaId).Max() + 1;
+            categorias.Add(categoria);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ progamaweb/progama/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="progama.MvcApplication" Language="C#" %>
