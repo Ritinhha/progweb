2853a3484dbe18acdd3d422fc4b39158db4b7896
Rita de Cássia Souza de Moura <20201011110020@ifrn.local>
2023-09-19 18:03:15 -0300

    aaa

---
+++ aula017/WebAppProjeto/WebAppProjetoB2023/Areas/tabelas/tabelasAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebAppProjetoB2023.Areas.tabelas
+{
+    public class tabelasAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "tabelas";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "tabelas_default",
+                "tabelas/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
