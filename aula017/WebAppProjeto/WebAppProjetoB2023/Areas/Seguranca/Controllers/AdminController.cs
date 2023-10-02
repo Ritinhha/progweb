38bf9a7d6d566244b3d0d10315847def974feb43
Rita de Cássia Souza de Moura <20201011110020@ifrn.local>
2023-10-02 15:57:34 -0300

    coisas

---
+++ aula017/WebAppProjeto/WebAppProjetoB2023/Areas/Seguranca/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppProjetoB2023.Areas.Seguranca.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Seguranca/Admin
+        private GerenciadorUsuario GerenciadorUsuario
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<GerenciadorUsuario>();
+
+            }
+        }
+        // GET: Seguranca/Admin
+        public ActionResult Index()
+        {
+            return View(GerenciadorUsuario.Users);
+        }
+    }
+}
